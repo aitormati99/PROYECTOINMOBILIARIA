1ddb1d6e8c8aae53be7c0c0b2ee732518f2cdab5
aitormati99 <aitor.matilla@opendeusto.es>
2020-10-26 15:54:26 +0100

    exception

---
+++ src/exception/UsuarioNoExiste.java
@@ -0,0 +1,19 @@
+package exception;
+
+import java.awt.List;
+
+/**
+ * @author aitor y ane Este es el paquete donde guardamos las excepciones, en
+ *         este caso la excepcion de UsuarioNoExiste
+ *
+ */
+public class UsuarioNoExiste extends Exception {
+
+	public UsuarioNoExiste(String mensaje) {
+		super(mensaje);
+		
+	}
+	
+	
+
+}
